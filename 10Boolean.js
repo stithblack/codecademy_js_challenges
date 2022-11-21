b8f56f3a9fc3ce2a224f833951fdfb6ec93dce46
Francis Blackwell Stith Jr <blackwell.stith@gmail.com>
2022-11-21 14:04:29 -0500

    Create 10Boolean.js

---
+++ 10Boolean.js
@@ -0,0 +1,9 @@
+//https://www.codecademy.com/resources/blog/10-javascript-code-challenges-for-beginners/
+//number 9
+
+input = prompt("Enter a number to see if it's divisible by 10:");
+
+if (input % 10 === 0) {
+  console.log("Your number is divisible by 10.");
+}
+else console.log("Your number is not divisible by 10.");
