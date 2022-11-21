12407a8053d04cf05a4fc23e4be6233dc2abfff7
Francis Blackwell Stith Jr <blackwell.stith@gmail.com>
2022-11-21 11:32:40 -0500

    Create removeSpaces.js

---
+++ removeSpaces.js
@@ -0,0 +1,7 @@
+//https://www.codecademy.com/resources/blog/10-javascript-code-challenges-for-beginners/
+//number 8
+
+const input = prompt("Input two words, separated by spaces.");
+var compound = input.replace(/ /g, '');
+
+console.log("The compound word is", compound);
