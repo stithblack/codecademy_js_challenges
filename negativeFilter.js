f924b7a64df5baba2d148bf442876c45f88bc400
Francis Blackwell Stith Jr <blackwell.stith@gmail.com>
2022-11-21 10:16:06 -0500

    Update negativeFilter.js

--- negativeFilter.js
+++ negativeFilter.js
@@ -1,8 +1,6 @@
 //https://www.codecademy.com/resources/blog/10-javascript-code-challenges-for-beginners/
 //number 7
 
-numList = [1,-2,3,-4,5]
-numlist = jQuery.grep(numlist, function( n, i ) {
-  return n >= 0;
-});
-console.log(numList)
+const numList = [1,-2,3,-4,5];
+const positiveNum = numList.filter(x => x >= 0);
+console.log(positiveNum);
